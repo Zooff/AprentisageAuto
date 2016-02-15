46113b2c414eb79b98d18140591ff4352b559cf7
Ludovic <ludovictichit3@gmail.com>
2016-02-15 14:01:58 +0100

    first ludo commit

--- src/graphicalInterface/Main.java
+++ src/graphicalInterface/Main.java
@@ -5,11 +5,13 @@ import freeman.TrainingData;
 public class Main {
 
 	public static void main(String[] args) {
+        
 		// TODO Auto-generated method stub
 		
 		Constant.TDATA = new TrainingData();
 		Constant.TDATA.Load("resources/test.txt");
 		//System.out.println(train.data);
+        
 		MainWindow main = new MainWindow();
 	}
 
